f7d82cc3f07a833f2417efcdd55a52783e50a6b0
fikri <deanradityo@gmail.com>
2021-07-12 02:24:16 +0700

    mengubah html ke php

--- home.php
+++ home.php
@@ -1,100 +1,102 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/index.css">
-    <title>Dean's Portfolio</title>
-  </head>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <body>
-    <!-- Navbar Start -->
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
-        <div class="container" id="navbarsatu">
-          <a href="#"><img class="navbar-brand" src="img/Brand - Foxico2.png" alt="brand"></a>
-          <a class="navbar-brand" href="#">FOXICO</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#portfolio" class="nav-link">Portfolio</a>
-                </li>
-            </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <button class="btn btn-primary my-2 my-sm-0 login " type="submit">Register</button>
-            <button class="btn btn-outline-success my-2 my-sm-0 login" type="submit">Login</button>
-          </form>
-        </div>
-      </nav>
-    <!-- Navbar End -->
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Dean's Portfolio</title>
+</head>
+
+<body>
+  <!-- Navbar Start -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
+    <div class="container" id="navbarsatu">
+      <a href="#"><img class="navbar-brand" src="img/Brand - Foxico2.png" alt="brand"></a>
+      <a class="navbar-brand" href="#">FOXICO</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="#portfolio" class="nav-link">Portfolio</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <button class="btn btn-primary my-2 my-sm-0 login " type="submit">Register</button>
+          <button class="btn btn-outline-success my-2 my-sm-0 login" type="submit">Login</button>
+        </form>
+      </div>
+  </nav>
+  <!-- Navbar End -->
 
 
-    <!-- Jumbotron Start -->
-    <div class="jumbotron text-center">
-        <img src="img/deanfix3.png" class="rounded-circle" class="text-center">
-        <h1>
-          Fikri Dean Radityo
-        </h1>
-        <p>
-          Web Developer | College Student | Web Designer
-        </p>
-        <div class="container">
-          <div class="row text-center">
-            <div class="col-sm-12 brand-dean">
-              <img src="img/Brand - Foxico2.png" alt="brand-foxico">
-              <h4>FOXICO Developer</h4>
-            </div>
-          </div>
-        </div>
-    </div>
-    <!-- Jumbotron End -->
-
-    <!-- About Start -->
-    <section class="about" id="about">
-      <div class="container">
-        <div class="row  text-center">
-          <div class="col-lg-12 text-center">
-            <h2>
-              What is a web developer?
-            </h2>
-            <hr>
-          </div>
-        </div> 
-        <div class="row justify-content-center">
-          <div class="col-lg-6">
-          <p>
-            A <span>web developer</span> is a programmer who specializes in, or is specifically engaged in, the development of World Wide Web applications using a client–server model. <span>Web developers</span> are found working in various types of organizations, including large corporations and governments, small and medium-sized companies, or alone as freelancers.
-            </p>
-          </div>
-          <div class="col-lg-6">
-            <p>
-              The applications typically use <span>HTML</span> , <span>CSS</span>  and <span>JavaScript</span>  in the client, <span>PHP</span>,<span>ASP.NET (C#)</span>, <span>Python</span>, <span>Node.js</span>, <span>Go</span></span> or <span>Java</span> in the server, and <span>http</span>, for communications between client and server. 
-            </p>
-          </div>
+  <!-- Jumbotron Start -->
+  <div class="jumbotron text-center">
+    <img src="img/deanfix3.png" class="rounded-circle" class="text-center">
+    <h1>
+      Fikri Dean Radityo
+    </h1>
+    <p>
+      Web Developer | College Student | Web Designer
+    </p>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-sm-12 brand-dean">
+          <img src="img/Brand - Foxico2.png" alt="brand-foxico">
+          <h4>FOXICO Developer</h4>
         </div>
       </div>
-    </section>
-    <!-- About End -->
+    </div>
+  </div>
+  <!-- Jumbotron End -->
 
-    <!-- Portfolio Start -->
-    <section class="portfolio" id="portfolio">
-      <div class="container">
-        <div class="row text-center">
-          <div class="col-lg-12">
-            <h2>
-              Try Out
-            </h2>
-          </div>
-            <hr>
+  <!-- About Start -->
+  <section class="about" id="about">
+    <div class="container">
+      <div class="row  text-center">
+        <div class="col-lg-12 text-center">
+          <h2>
+            What is a web developer?
+          </h2>
+          <hr>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <p>
+            A <span>web developer</span> is a programmer who specializes in, or is specifically engaged in, the development of World Wide Web applications using a client–server model. <span>Web developers</span> are found working in various types of organizations, including large corporations and governments, small and medium-sized companies, or alone as freelancers.
+          </p>
+        </div>
+        <div class="col-lg-6">
+          <p>
+            The applications typically use <span>HTML</span> , <span>CSS</span> and <span>JavaScript</span> in the client, <span>PHP</span>,<span>ASP.NET (C#)</span>, <span>Python</span>, <span>Node.js</span>, <span>Go</span></span> or <span>Java</span> in the server, and <span>http</span>, for communications between client and server.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- About End -->
+
+  <!-- Portfolio Start -->
+  <section class="portfolio" id="portfolio">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-lg-12">
+          <h2>
+            Try Out
+          </h2>
+        </div>
+        <hr>
         <div class="container">
           <div class="row mx-auto align-items-center justify-content-center">
             <div class="col-lg-4">
@@ -119,8 +121,8 @@
                   <p class="card-text">My First Design //
                     <br>
                     <a href="https://mail.google.com/mail/?view=cm&fs=1&to=deanradityo@gmail.com&su=SUBJECT&body=BODY&bcc=deanradityo@gmail.com" target="_blank">Foxico's Email<a> //
-                    <br>
-                    <a href="https://dribbble.com/shots/15607887-StepWorld" target="_blank">StepWorld Design</a> //
+                        <br>
+                        <a href="https://dribbble.com/shots/15607887-StepWorld" target="_blank">StepWorld Design</a> //
                   </p>
                 </div>
               </div>
@@ -141,11 +143,11 @@
             </div>
           </div>
         </div>
-    </section>
-    <!-- Portfolio End -->
+  </section>
+  <!-- Portfolio End -->
 
-    <!-- Contact Start -->
-    <!-- <section class="contact" id="contact">
+  <!-- Contact Start -->
+  <!-- <section class="contact" id="contact">
       <div class="container">
         <div class="row">
           <div class="col-sm-12 text-center">
@@ -158,8 +160,8 @@
 
         <div class="container">
           <div class="row justify-content-center"> -->
-        <!-- Helper Start -->
-            <!-- <div class="col-sm-4">
+  <!-- Helper Start -->
+  <!-- <div class="col-sm-4">
               <h1>
                 SIX
               </h1>
@@ -173,12 +175,12 @@
                 <div class="card-header text-center"><h6 class="text-left engga">Location</h6></div>
               </div>
             </div> -->
-        <!-- Helper End -->
-        <!-- Middle Side Start -->
-        <!-- <div class="col-sm-1"></div> -->
-        <!-- Middle Side End -->
-        <!-- Form Start -->
-            <!-- <div class="col-sm-5 formkanan">
+  <!-- Helper End -->
+  <!-- Middle Side Start -->
+  <!-- <div class="col-sm-1"></div> -->
+  <!-- Middle Side End -->
+  <!-- Form Start -->
+  <!-- <div class="col-sm-5 formkanan">
               <div class="card cardcontact">
                 <div class="card-body">
                   <form>
@@ -220,53 +222,57 @@
             </div>
           </div>
         </div> -->
-        <!-- form End -->
-      <!-- </div>
+  <!-- form End -->
+  <!-- </div>
     </section> -->
-    <!-- Contact End -->
+  <!-- Contact End -->
 
-    <!-- Pull Up Start -->
-    <section class="pull-up" id="pull-up">
-      <div class="container">
-        <div class="row  text-center">
-          <div class="col-lg-12 text-center">
-              <a class="top-link hide" href="" id="js-top">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg>
-                <span class="screen-reader-text">Back to top</span>
-              </a>
-          </div>
+  <!-- Pull Up Start -->
+  <section class="pull-up" id="pull-up">
+    <div class="container">
+      <div class="row  text-center">
+        <div class="col-lg-12 text-center">
+          <a class="top-link hide" href="" id="js-top">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6">
+              <path d="M12 6H0l6-6z" />
+            </svg>
+            <span class="screen-reader-text">Back to top</span>
+          </a>
         </div>
       </div>
-    </section>
-    <!-- Pull Up End -->
+    </div>
+  </section>
+  <!-- Pull Up End -->
 
-    <!-- Footer Start -->
-    <footer>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-sm-12">
-            <p>
-              &copy; 2020 - 2021 <a href="">FOXICO</a> • All rights reserved • Love from Depok
-            </p>
-          </div>
+  <!-- Footer Start -->
+  <footer>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-sm-12">
+          <p>
+            &copy; 2020 - 2021 <a href="">FOXICO</a> • All rights reserved • Love from Depok
+          </p>
         </div>
       </div>
-    </footer>
-    <!-- Footer End -->
+    </div>
+  </footer>
+  <!-- Footer End -->
 
 
 
-    <!-- Optional JavaScript; choose one of the two! -->
+  <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <script src="js/script.js"></script>
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
+  <script src="js/script2.js"></script>
 
-    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+  <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
