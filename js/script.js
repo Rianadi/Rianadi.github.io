ce61f99667c6bcc00ef827f350e4b667499db4ba
fikdean <fikdean@gmail.com>
2021-08-22 14:50:03 +0700

    mengganti title web

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// Navbar
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
@@ -7,6 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, options);
 });
+
+
 
 // const nama = document.getElementsByClassName('.nama');
 // const huruf = [...nama.textContent].map(m => `<span>${m}</span>`).join('');
