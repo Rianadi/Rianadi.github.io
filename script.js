68e6b3a7f6b0cd848371f1b1ec9910ab2281978d
fikdean <fikdean@gmail.com>
2022-01-15 22:16:59 +0700

    Menambahkan icon2 di berbagai class dan menambahkan icon silang pada responsive navbar

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
+  var y = document.getElementById("icon");
   if (x.className === "topnav") {
     x.className += " responsive";
+    y.innerHTML = `<i class="fa fa-times-circle" style="font-size:24px" aria-hidden="true"></i>`;
   } else {
     x.className = "topnav";
+    y.innerHTML = `<i class="fa fa-bars" style="font-size:24px" aria-hidden="true"></i>`;
   }
 }
