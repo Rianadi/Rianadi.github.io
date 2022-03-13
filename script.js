d7db13bb40be17585245935e202f3f3aaff924aa
fikdean <fikdean@gmail.com>
2022-03-14 00:33:44 +0700

    Mengubah seluruh web

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-function hamburgerMenu() {
-  const firstClass = $('#myTopnav').hasClass('topnav');
-  const secondClass = $('#myTopnav').hasClass('responsive');
-
-  if (firstClass && !(secondClass)) {
-    $('#myTopnav').toggleClass('responsive');
-
-  } else if (firstClass && secondClass) {
-    $('#myTopnav').toggleClass('responsive');
-    
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+const certificateMobile = document.querySelectorAll('.certificate-mobile');
+const projectMobile = document.querySelectorAll('.project-mobile');
+const certificateWeb = document.querySelectorAll('.certificate-web');
+const projectWeb = document.querySelectorAll('.project-web');
+
+if (screenWidth > 768) {
+  for(let i = 0; i < certificateWeb.length; i++) {
+    certificateMobile[i].style.display = 'none';
+  }
+  for(let i = 0; i < certificateWeb.length; i++) {
+    projectMobile[i].style.display = 'none';
+  }
+} else {
+  
+  for(let i = 0; i < certificateWeb.length; i++) {
+    certificateWeb[i].style.display = 'none';
+  }
+
+  for(let i = 0; i < projectWeb.length; i++) {
+    projectWeb[i].style.display = 'none';
+  }
+  
 }
