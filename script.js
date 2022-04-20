68443e7ed0bb15adc2674a5a378d64027210432f
fikdean <fikdean@gmail.com>
2022-04-21 01:18:40 +0700

    Menghilangkan page welcome dan mengubah tinggi dari class card

--- script.js
+++ script.js
@@ -19,72 +19,6 @@ $( document ).ready(function() {
     }
   }
 
-  // Certificate left button
-  $('#cerLeft').on('click', function() {
-    const numChange = 200;
-    let numLeft = $(".card-web").scrollLeft() - numChange;
-    $('.certificate-web').animate({scrollLeft: numLeft}, 300);
-  });
-
-  // Certificate right button
-  $('#cerRight').on('click', function() {
-    const numChange = 200;
-    let numRight = $(".card-web").scrollLeft() + numChange;
-    $('.certificate-web').animate({scrollLeft: numRight}, 300);
-  });
-
-  // Project left button
-  $('#proLeft').on('click', function() {
-    const numChange = 200;
-    let numLeft = $(".project-web").scrollLeft() - numChange;
-    $('.project-web').animate({scrollLeft: numLeft}, 300);
-  });
-
-  // Project right button
-  $('#proRight').on('click', function() {
-    const numChange = 200;
-    let numRight = $(".project-web").scrollLeft() + numChange;
-    $('.project-web').animate({scrollLeft: numRight}, 300);
-  });
-
-  $('.button-welcome').on({
-    click: function() {
-      $('.main').html(`<main class='contact-value' id="contact">
-                          <div class="row">
-                            <div class='col-12 col-s-12'>
-                              <h1><a href="javascript:;">Fikri Dean Radityo</a></h1>
-                              <h2><a href="javascript:;" class="job">Web Developer</a></h2>
-                              <h3><a href="javascript:;">rianadi.github.io</a></h3>
-                              <div class="row btn">
-                                <div class="col-3 col-s-12">
-                                  <input type="button" onclick="email()" formtarget="_blank" class='email' value="Email">
-
-                                  <!-- <button class='email'><a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSBptCNkzXPGKQFhFQflhqSTFvRPLDPfnfkrRTJscwSZgRdNDhKwPHhSdVjJPrbcxhzQZTlW" target="_blank"><i class="fa-solid fa-envelope" target="_blank"></i> Email</a></button> -->
-                                </div>
-                                <div class="col-3 col-s-12">
-                                  <input type="button" onclick="linkedin()" formtarget="_blank" class='linked' value="LinkedIn">
-
-                                  <!-- <button class='linked'><a href="https://www.linkedin.com/in/fikri-dean-radityo-23bb3621a/" target="_blank"><i class="fa-brands fa-linkedin"></i> LinkedIn</a></button> -->
-                                </div>
-                                <div class="col-3 col-s-12">
-                                  <input type="button" onclick="github()" formtarget="_blank" class='github' value="Github">
-
-                                  <!-- <button class='github'><a href="https://github.com/Rianadi" target="_blank"><i class='fa fa-github-square'></i> Github</a></button> -->
-                                </div>
-                                <div class="col-3 col-s-12">
-                                  <input type="button" onclick="freecodecamp()" formtarget="_blank" class='freecodecamp' value="FreeCodeCamp">
-
-                                  <!-- <button class='freecodecamp'><a href="https://www.freecodecamp.org/fikridean" target="_blank"><i class='fa fa-free-code-camp'></i> FreeCodeCamp</a></button> -->
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </main>`);
-      $('.nav ul li a.contact-value').addClass('active');
-      $('.home').removeClass('active');
-    }
-  })
-
   $('.nav ul li a').on({
     click: function() {
       $('nav ul .active').removeClass('active');
@@ -114,19 +48,6 @@ $( document ).ready(function() {
                                 </div>
                               </div>
                         </main>`);
-      } else if ($(this).hasClass('home')){
-          $('.main').html(`<div class="welcome" data-aos="zoom-in">
-                            <div class="row">
-                              <div class="col-12 col-s-12">
-                                <h1>Welcome To My Portfolio WebPage!</h1>
-                              </div>
-                            </div>
-                            <div class="row">
-                              <div class="col-12 col-s-12">
-                                <input type="button" formtarget="_blank" class='btn button-welcome' value="Look Around">
-                              </div>
-                            </div>
-                          </div>`);
       } else if ($(this).hasClass('personal')) {
         $('.main').html(`<div data-aos="zoom-in">
                           <div class="row intro" id="intro">
